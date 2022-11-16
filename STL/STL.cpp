c26d9f621d204aa4c1ca857dfdc23daf02f9ac9a
chanho <chanho@DESKTOP-3RDBK2C>
2022-11-16 22:06:44 +0900

    auto, algorithm use

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -4,6 +4,7 @@
 #include<deque>
 #include<map>
 #include<set>
+#include<algorithm>
 
 using namespace std;
 
@@ -19,12 +20,15 @@ int main()
 	}
 
 	// 벡터안에 number가 있는지 찾는 기능
+	int number = 50;
 	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
 	{
-		int number = 50;
+		
 		if (*it == number)
 			cout << "right!" <<" " << *it<< endl;
 	}
+
+	auto itFind = find(v.begin(), v.end(), number);
 	
 	//벡터 안에 11의 배수가 있는지 찾는 기능
 	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
@@ -34,6 +38,8 @@ int main()
 		
 	}
 	
+	auto ifFind = find_if(v.begin(), v.end(), [](int n) {return n % 11 == 0; });
+
 	//벡터 안에 홀수를 찾는 기능
 	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
 	{
@@ -45,10 +51,24 @@ int main()
 		}
 	}
 
+	auto ifFind = find_if(v.begin(), v.end(), [](int n) { return n % 2 != 0; });
+
 	//벡터 안의 값들에 3을 곱하는 기능
 	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
 	{
 		cout << *it << " 3배: " << (*it) * 3 << endl;
 	}
 
+	auto itMul = for_each(v.begin(), v.end(), [](int n) {return n = n * 3; });
+
+	// 홀수인 데이터 삭제하는 기능
+	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+	{
+		if ((*it % 2) != 0)
+			it = v.erase(it);
+		
+	}
+
+	
+	v.erase(remove_if(v.begin(), v.end(), [](int n) {return n % 2 != 0; }), v.end());
 }
